81679ecd503a0ec91f696c966fe840575f960a45
Lars Werner <lars.waernulf+5@gmail.com>
2015-03-24 13:00:52 +0100

    hämta data3

--- server.js
+++ server.js
@@ -82,10 +82,13 @@ app.get('/show', function (req, res) {
         }
         res.writeHead(200, { 'content-type': 'text/plain' });
         var resultat = result.entries;
+        res.write(JSON.stringify(resultat[0]) + '\n\n');
+        /*
         for (var r = 0; r < 5; r++) {
             res.write(resultat[r].imgURL['_'] + '</br>');
             res.write('</br>');
         }
+        */
         res.end();
     });
 });
